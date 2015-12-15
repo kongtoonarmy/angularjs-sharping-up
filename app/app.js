aa880b0e7a0ffbcf7f886822c293ef17832e098e
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2015-12-15 18:11:17 +0700

    How to use Controller in AngularJS

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('gemStore', []);
+var app = angular.module('store', []);
+app.controller('StoreController', ['$scope', function($scope) {
+    $scope.product = gem;
+}]);
+
+var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'Description of Dodecahedron'
+};
