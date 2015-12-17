a3cf357a8e22d1eb23581667e6b6e6da2a35bf60
kongtoonarmy <kongtoonarmy@hotmail.com>
2015-12-17 13:57:30 +0700

    How to use "ng-src"

--- app/app.js
+++ app/app.js
@@ -9,27 +9,38 @@ var gem = [
         price: 16790.95,
         description: 'Description of iPad Mini 2',
         canPurchase: true,
-        soldOut: true
+        soldOut: true,
+        images: [
+            {
+                full: 'ipad-mini2.jpg',
+                thumb: 'ipad-mini2.jpg'
+            }
+        ]
     },
     {
         name: 'iPhone 6s Plus',
         price: 29000,
         description: 'Description of iPhone 6s Plus',
         canPurchase: false,
-        soldOut: false
+        soldOut: false,
+        images: [
+            {
+                full: 'iphone6s-plus.jpg',
+                thumb: 'iphone6s-plus.jpg'
+            }
+        ]
     },
     {
         name: 'Nexus 5',
         price: 18900.3567,
         description: 'Description of Nexus 5',
         canPurchase: true,
-        soldOut: false
-    },
-    {
-        name: 'Samsung Galaxy S6',
-        price: 22990.4,
-        description: 'Description of Samsung Galaxy S6',
-        canPurchase: true,
-        soldOut: false
+        soldOut: false,
+        images: [
+            {
+                full: 'nexus5.jpg',
+                thumb: 'nexus5.jpg'
+            }
+        ]
     }
 ];
