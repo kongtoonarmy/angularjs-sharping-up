7b7f0ba7eaf2e69d089fa6a92198c3f6d0a22b04
kongtoonarmy <kongtoonarmy@hotmail.com>
2015-12-17 16:45:00 +0700

    How to build tab by using Angular + Bootstrap

--- app/app.js
+++ app/app.js
@@ -1,46 +1,13 @@
-var app = angular.module('store', []);
-app.controller('StoreController', function() {
-    this.products = gem;
-});
+var app = angular.module('myApp', []);
+app.controller('PanelController', function() {
+    this.tab = 1;
 
-var gem = [
-    {
-        name: 'iPad Mini 2',
-        price: 16790.95,
-        description: 'Description of iPad Mini 2',
-        canPurchase: true,
-        soldOut: true,
-        images: [
-            {
-                full: 'ipad-mini2.jpg',
-                thumb: 'ipad-mini2.jpg'
-            }
-        ]
-    },
-    {
-        name: 'iPhone 6s Plus',
-        price: 29000,
-        description: 'Description of iPhone 6s Plus',
-        canPurchase: false,
-        soldOut: false,
-        images: [
-            {
-                full: 'iphone6s-plus.jpg',
-                thumb: 'iphone6s-plus.jpg'
-            }
-        ]
-    },
-    {
-        name: 'Nexus 5',
-        price: 18900.3567,
-        description: 'Description of Nexus 5',
-        canPurchase: true,
-        soldOut: false,
-        images: [
-            {
-                full: 'nexus5.jpg',
-                thumb: 'nexus5.jpg'
-            }
-        ]
-    }
-];
+    this.selectTab = function(setTab) {
+        this.tab = setTab;
+        console.log(this.tab);
+    };
+
+    this.isSelected = function(checkTab) {
+        return this.tab === checkTab;
+    };
+});
